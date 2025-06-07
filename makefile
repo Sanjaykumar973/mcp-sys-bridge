b3cb5de9ebcb1ad930baeb5c97385bc3895bed7f
Leynier Gutiérrez González <leynier41@gmail.com>
2025-06-06 20:37:17 -0600

    feat: implement the first version

--- makefile
+++ makefile
@@ -1,2 +1,6 @@
 install:
 	uv sync --all-groups --all-extras
+
+tests: install
+	uv run ruff check .
+	uv run ruff format --check .
--- src/server.py
+++ src/server.py
@@ -1,3 +1,6 @@
+import webbrowser
+
+import pyperclip
 from fastmcp import FastMCP
 
 mcp = FastMCP(
@@ -7,13 +10,27 @@ mcp = FastMCP(
 
 
 @mcp.tool()
-async def open_url(url: str) -> None:
-    pass
+async def open_url(url: str) -> str:
+    """
+    Open a URL in the default browser.
+    """
+    try:
+        webbrowser.open(url)
+        return "URL opened successfully."
+    except Exception as e:
+        return f"Error opening URL: {e}"
 
 
 @mcp.tool()
-async def copy_to_clipboard(text: str) -> None:
-    pass
+async def copy_to_clipboard(text: str) -> str:
+    """
+    Copy text to the clipboard.
+    """
+    try:
+        pyperclip.copy(text)
+        return "Text copied to clipboard successfully."
+    except Exception as e:
+        return f"Error copying text to clipboard: {e}"
 
 
 def main() -> None:
